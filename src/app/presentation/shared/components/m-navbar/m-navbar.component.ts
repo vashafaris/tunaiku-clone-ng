9a31b2a7130b35cb67b1b845088dd2241acbafa6
Vasha Farisi <vashafaris@gmail.com>
2020-11-09 13:06:06 +0700

    dynamic header

--- src/app/presentation/shared/components/m-navbar/m-navbar.component.ts
+++ src/app/presentation/shared/components/m-navbar/m-navbar.component.ts
@@ -1,5 +1,5 @@
 import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-m-navbar',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./m-navbar.component.scss'],
 })
 export class MNavbarComponent implements OnInit {
+  @Input() title: string;
   constructor(private location: Location) {}
 
   ngOnInit(): void {}
